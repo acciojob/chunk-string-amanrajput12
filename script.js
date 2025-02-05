d18361e628fc52b4174d78c4c435babc7b1c9051
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-02-05 13:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function stringChop(str, size) {
   // your code here
+	let value=Number(size);
+	let rst=[];
+		for(let i=0;i<str.length;i+=value){
+         rst.push(str.substring(i,i+value));
+		}
+	return rst
 }
 
 // Do not change the code below
